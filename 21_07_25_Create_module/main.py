4c4b30074ea68d84e0cc23556ebe54c11f15ee35
Cabuxos <velichko.kir@gmail.com>
2025-07-21 20:20:53 +0200

    Initial commit

---
+++ 21_07_25_Create_module/main.py
@@ -0,0 +1,8 @@
+from utility import tourette, cis_region_end_bracket, nothing_happened_in_tiananmen_in_1989 
+
+print(tourette("I'm Alex."))
+print(cis_region_end_bracket(1))
+print(cis_region_end_bracket("I'm Alex."))
+print(cis_region_end_bracket("I'm Alex!"))
+print(nothing_happened_in_tiananmen_in_1989("THEY KILLED STUDENTS IN TIANANMEN SQUARE IN 1989!!"))
+print(nothing_happened_in_tiananmen_in_1989("1989 is the best year so far!"))
